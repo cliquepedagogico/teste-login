0346cfa8ae7a7616507554a15fdb26f38db3a1fb
robsonrdev <robsonrdscontato@gmail.com>
2025-03-20 18:55:38 -0300

    Testando

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const password = document.querySelector("input[name='password']").value;
 
         try {
-            const response = await fetch("http://127.0.0.1:5002/api/login", {
+            const response = await fetch("https://cliquepedagogico.github.io/api/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
